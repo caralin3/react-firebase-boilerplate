ae0c0ab511b63467b89ab2d0d611fb96f5b2a308
caralin3 <caralin3@gmail.com>
2018-08-08 15:44:39 -0400

    Added notes for production versus dev configuration

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,6 +3,28 @@ import 'firebase/auth';
 import 'firebase/database';
 import { config } from '../firebase_config'
 
+// const prodConfig = {
+//   apiKey: YOUR_API_KEY,
+//   authDomain: YOUR_AUTH_DOMAIN,
+//   databaseURL: YOUR_DATABASE_URL,
+//   projectId: YOUR_PROJECT_ID,
+//   storageBucket: '',
+//   messagingSenderId: YOUR_MESSAGING_SENDER_ID,
+// };
+
+// const devConfig = {
+//   apiKey: YOUR_API_KEY,
+//   authDomain: YOUR_AUTH_DOMAIN,
+//   databaseURL: YOUR_DATABASE_URL,
+//   projectId: YOUR_PROJECT_ID,
+//   storageBucket: '',
+//   messagingSenderId: YOUR_MESSAGING_SENDER_ID,
+// };
+
+// const config = process.env.NODE_ENV === 'production'
+//   ? prodConfig
+//   : devConfig;
+
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
 }
